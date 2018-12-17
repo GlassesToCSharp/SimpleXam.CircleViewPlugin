cac2b8b31623935e32b322eaded54d54dba7732c
Tom <t.bartleet@hotmail.co.uk>
2018-12-17 09:06:44 +0000

    First Commit
    First Commit

---
+++ SimpleXam.CircleViewPlugin/SimpleXam.CircleViewPlugin.iOS/CircleViewRenderer.cs
@@ -0,0 +1,25 @@
+﻿using SimpleXam.CircleViewPlugin;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CircleView), typeof(CircleViewRenderer))]
+namespace SimpleXam.CircleViewPlugin
+{
+    public class CircleViewRenderer : BoxRenderer
+    {
+        private CircleView CustomElement => Element as CircleView;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<BoxView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Element == null)
+            {
+                return;
+            }
+
+            Layer.MasksToBounds = true;
+            Layer.CornerRadius = (float)CustomElement.Radius / 2.0f;
+        }
+    }
+}
